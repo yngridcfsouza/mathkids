a699a0f0219d229575472e1cb807dab1942db118
Yngrid F. Souza <isnotlive.yngrid@gmail.com>
2025-12-04 17:13:35 -0300

    Deploy

--- docs/screenshots/README.md
+++
@@ -1,2 +0,0 @@
-Adicione aqui as imagens de screenshots.
-Nomes sugeridos: home.png, exercise.png, result.png.
--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: './',
+  build: {
+    outDir: '../docs',
+    emptyOutDir: false,
+  },
 })
